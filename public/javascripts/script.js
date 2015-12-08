e2bb0c67f3ae920da391af4b141ac68f8b849284
imarkwick <izzymarkwick@gmail.com>
2015-12-08 16:25:59 +0000

    broken sound

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,12 +3,31 @@ $(document).ready(function() {
   SC.initialize({
     client_id: 'aa7a4eb3979426038d8aa53a458baa54'
   })
+  
+  var track1 = SC.stream('/tracks/236619719');
 
-  // $(document).click(function() {
-  //   SC.stream('/tracks/236619719').then(function(player) {
-  //     player.play();
-  //   });
-  // });
+  $('#m1').click(function() {
+    
+    if (track1.isPaused(player)) {
+      console.log('I know its paused');
+      track1.then(function(player) {
+        player.play();
+      });
 
+    } else {
 
+      console.log('HIHIHIHIHI');
+      track1.then(function(player) {
+        player.pause();
+      });
+
+    };
+
+  });
+
+  $('#e').click(function() {
+    track1.then(function(player) {
+      player.pause();
+    });
+  });
 });
