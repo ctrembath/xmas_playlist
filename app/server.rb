632f0150e4f5f76fd2562efa2fe26bcc12bb6b78
imarkwick <izzymarkwick@gmail.com>
2015-12-08 15:45:20 +0000

    soundcloud - ruby and javascript api play

--- app/server.rb
+++ app/server.rb
@@ -1,7 +1,11 @@
 require 'sinatra'
+require 'soundcloud'
 
 set :public_dir, Proc.new { File.join(root, "..", "public") }
 
 get '/' do
   erb :index
 end
+
+private
+
---
+++ public/javascripts/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  SC.initialize({
+    client_id: 'aa7a4eb3979426038d8aa53a458baa54'
+  })
+
+  // $(document).click(function() {
+  //   SC.stream('/tracks/236619719').then(function(player) {
+  //     player.play();
+  //   });
+  // });
+
+
+});
