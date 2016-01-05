a308298d00613a67e946ff117355fc72791f6ac0
imarkwick <izzymarkwick@gmail.com>
2016-01-05 17:32:28 +0000

    soundmanager not yet playing but installed correctly with second version

--- public/javascripts/soundmanagerSetup.js
+++ public/javascripts/soundmanagerSetup.js
@@ -1,15 +1,22 @@
 $(document).ready(function() {
 
   soundManager.setup({
-    url: '/javascripts/swf/',
+    url: './swf',
+    flashVersion: 9,
     onready: function() {
-      console.log('hi from in the soundmanager onready method');
-      var mySound = soundManager.createSound({
-        url: 'https://api.soundcloud.com/tracks/233605980?client_id=aa7a4eb3979426038d8aa53a458baa54',
-        onload: function() { console.log('sound has loaded'); }
-      });
-      console.log(mySound);
-      // mySound.play();
+      console.log('soundmanager ready to create a sound');
+      createToPlay();
     }
   });
+
+  var createToPlay = function() {
+    var mySound = soundManager.createSound({
+      id: 'testingSoundID',
+      url: 'https://api.soundcloud.com/tracks/233605980?client_id=aa7a4eb3979426038d8aa53a458baa54'
+    });
+    console.log(mySound);
+    console.log(mySound.url);
+    mySound.play();
+  };
+
 });
