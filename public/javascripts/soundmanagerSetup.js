83ab8efc2ef641d4370639f23dbbae476468de57
imarkwick <izzymarkwick@gmail.com>
2016-01-08 14:18:57 +0000

    refactoring soundmanager methods

--- public/javascripts/soundmanagerSetup.js
+++ public/javascripts/soundmanagerSetup.js
@@ -1,24 +1,39 @@
 $(document).ready(function() {
 
+  var soundcloudClientID = 'aa7a4eb3979426038d8aa53a458baa54';
+
   soundManager.setup({
     url: './swf',
     flashVersion: 9,
+
     onready: function() {
       
-      SC.initialize({
-        client_id: 'aa7a4eb3979426038d8aa53a458baa54'
-      });
+      initializeSoundcloud();
 
-      var streamURL = 'https://api.soundcloud.com/tracks/226404391/stream?client_id=aa7a4eb3979426038d8aa53a458baa54';
+      var streamURL = getURL('233605980');
 
-      soundManager.createSound({
-        id: 'track1',
-        url: streamURL
-      });
+      soundmanagerCreate('track1', streamURL);
 
       $('#m1').click(function() {
         soundManager.play('track1');
       });
     }
   });
+
+  var initializeSoundcloud = function() {
+    SC.initialize({
+      client_id: 'aa7a4eb3979426038d8aa53a458baa54'
+    });
+  };
+
+  var getURL = function(soundcloudTrackID) {
+    return 'https://api.soundcloud.com/tracks/' + soundcloudTrackID + '/stream?client_id=' + soundcloudClientID;
+  };
+
+  var soundmanagerCreate = function(trackID, trackURL) {
+    soundManager.createSound({
+      id: trackID,
+      url: trackURL
+    });
+  };
 });
