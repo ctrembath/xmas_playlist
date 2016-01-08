2e5a6616cea543eb413e00beadc2a04b6f885035
imarkwick <izzymarkwick@gmail.com>
2016-01-08 14:08:08 +0000

    audio working on mobile wooooo

--- public/javascripts/soundmanagerSetup.js
+++ public/javascripts/soundmanagerSetup.js
@@ -4,17 +4,14 @@ $(document).ready(function() {
     url: './swf',
     flashVersion: 9,
     onready: function() {
-      console.log('soundmanager is ready to create a sound');
       
       SC.initialize({
         client_id: 'aa7a4eb3979426038d8aa53a458baa54'
       });
 
-      var consumer_key = "aa7a4eb3979426038d8aa53a458baa54";
-
       var streamURL = 'https://api.soundcloud.com/tracks/226404391/stream?client_id=aa7a4eb3979426038d8aa53a458baa54';
 
-      var test = soundManager.createSound({
+      soundManager.createSound({
         id: 'track1',
         url: streamURL
       });
@@ -22,19 +19,6 @@ $(document).ready(function() {
       $('#m1').click(function() {
         soundManager.play('track1');
       });
-
     }
   });
-
-  var createToPlay = function() {
-    var mySound = soundManager.createSound({
-      id: 'testingSoundID',
-      autoLoad: true,
-      url: '../assets/Gramatik.mp3'
-    });
-    $('#m1').click(function() {
-      soundManager.play('testingSoundID', '../assets/Gramatik.mp3');
-    });
-  };
-
 });
