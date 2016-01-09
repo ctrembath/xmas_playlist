209e2c2e7eecb7bccfea150949222c507e6d7f34
imarkwick <izzymarkwick@gmail.com>
2016-01-09 18:14:12 +0000

    playing the playlist through when track finishes and removing media queries for track info and play info on mobile

--- public/javascripts/soundmanagerSetup.js
+++ public/javascripts/soundmanagerSetup.js
@@ -15,28 +15,28 @@ $(document).ready(function() {
         var streamURL = getURL('233605980');
         var letter = event.target;
         soundmanagerCreate('track1', streamURL);
-        playMerry(letter, 'track1', 'Couros - Turning')
+        playMerry(letter, 'track1', 'Couros - Turning', 'e')
       });
 
       $('#e').click(function() {
         var streamURL = getURL('225425321');
         var tune = event.target;
         soundmanagerCreate('track2', streamURL);
-        playMerry(tune, 'track2', 'DREWXHILL - Struggle')
+        playMerry(tune, 'track2', 'DREWXHILL - Struggle', 'r1')
       });
 
       $('#r1').click(function() {
         var streamURL = getURL('226404391');
         var tune = event.target;
         soundmanagerCreate('track3', streamURL);
-        playMerry(tune, 'track3', 'Camel Power Club - Ourson');
+        playMerry(tune, 'track3', 'Camel Power Club - Ourson', 'r2');
       });
 
       $('#r2').click(function() {
         var streamURL = getURL('229055409');
         var tune = event.target;
         soundmanagerCreate('track4', streamURL);
-        playMerry(tune, 'track4', 'Club Kuru - Layla');
+        playMerry(tune, 'track4', 'Club Kuru - Layla', 'y');
       });
 
       $('#y').click(function() {
@@ -50,6 +50,8 @@ $(document).ready(function() {
 
   });
 
+  // Initialize Soundcloud and get stream url
+
   var initializeSoundcloud = function() {
     SC.initialize({
       client_id: soundcloudClientID
@@ -60,6 +62,8 @@ $(document).ready(function() {
     return 'https://api.soundcloud.com/tracks/' + soundcloudTrackID + '/stream?client_id=' + soundcloudClientID;
   };
 
+  // SoundManager create and play methods
+
   var soundmanagerCreate = function(trackID, trackURL) {
     soundManager.createSound({
       id: trackID,
@@ -67,29 +71,47 @@ $(document).ready(function() {
     });
   };
 
-  var soundmanagerPlay = function(trackID) {
+  var soundmanagerPlay = function(trackID, nextLetter) {
+    var nextTrack = document.getElementById(nextLetter);
     soundManager.pauseAll();
     soundManager.play(trackID, {
       onfinish: function() {
         console.log('&& TRACK FINISHED &&');
+        nextTrack.click();
       }
     });
   };
+
+  // Track info in top banner
 
   var displayTrackInfo = function(trackName) {
     var infoBanner = document.getElementById('artist-info');
     infoBanner.innerHTML = trackName
   };
 
-  var playMerry = function(tune, soundmanagerTrackID, TrackInfo) {
+  // Play and pause tracks in MERRY
+
+  var playMerry = function(tune, soundmanagerTrackID, TrackInfo, nextLetter) {
     if (tune.className.indexOf('playing') === 6) {
-      soundmanagerPlay(soundmanagerTrackID);
+      soundmanagerPlay(soundmanagerTrackID, nextLetter);
       displayTrackInfo(TrackInfo);
     } else {
       soundManager.pause(soundmanagerTrackID);
       displayTrackInfo('');
     };
   };
+
+  // Play and pause tracks in CHRISTMAS
+
+  // var playChristmas = function(tune, soundmanagerTrackID, TrackInfo) {
+  //   if (tune.className.indexOf('playing') === 11) {
+  //     soundmanagerPlay(soundmanagerTrackID);
+  //     displayTrackInfo(TrackInfo);
+  //   } else {
+  //     soundManager.pause(soundmanagerTrackID);
+  //     displayTrackInfo('');
+  //   };
+  // };
 
 });
 
