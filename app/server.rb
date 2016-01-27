fea885576b619c32f9cc8a3744a34674818cd4ac
imarkwick <izzymarkwick@gmail.com>
2016-01-27 22:25:19 +0000

    method for choosing homepage to render depending on day of the week

--- app/server.rb
+++ app/server.rb
@@ -1,9 +1,11 @@
 require 'sinatra'
 require 'soundcloud'
+require 'date'
 
 set :public_dir, Proc.new { File.join(root, "..", "public") }
 
 get '/' do
+  # date_today
   erb :monday
 end
 
@@ -42,4 +44,40 @@ get '/sunday' do
 end
 
 private
+
+
+def date_today  
+  @date = Date.today
+  @day = @date.strftime('%A')
+
+  if @day == 'Monday'
+    puts 'MONDAY'
+    erb :monday
+  elsif @day == 'Tuesday'
+    puts 'TUESDAY'
+    erb :tuesday
+  elsif @day == 'Wednesday'
+    puts 'WEDNESDAY'
+    erb :wednesday
+  elsif @day == 'Thursday'
+    puts 'THURSDAY'
+    erb :thursday
+  elsif @day == 'Friday'
+    puts 'FRIDAY'
+    erb :friday
+  else
+    puts 'NO DAY'
+    erb :monday
+  end
+end
+
+
+
+
+
+
+
+
+
+
 
