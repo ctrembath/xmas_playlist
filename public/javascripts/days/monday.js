1bbe58ffab0da6f4db5debcfd39cc73b6b061c18
imarkwick <izzymarkwick@gmail.com>
2016-02-01 13:24:38 +0000

    refactoring

--- public/javascripts/days/monday.js
+++ public/javascripts/days/monday.js
@@ -11,102 +11,80 @@ $(document).ready(function() {
 
       $('#c').click(function() {
         var trackID = '235758881'
-        var streamURL = getURL(trackID);
         var tune = event.target;
-
-        soundmanagerCreate(trackID, streamURL);
+        soundmanagerCreate(trackID);
         play(tune, trackID, 'Ray BLK - 50/50', 'h');
       });
 
       $('#h').click(function() {
         var trackID = '187929420'
-        var streamURL = getURL(trackID);
         var tune = event.target;
-
-        soundmanagerCreate(trackID, streamURL);
+        soundmanagerCreate(trackID);
         play(tune, trackID, 'Ménage à Trois - Ocean Boy', 'r3');
       });
 
       $('#r3').click(function() {
         var trackID = '24895618'
-        var streamURL = getURL(trackID);
         var tune = event.target;
-
-        soundmanagerCreate(trackID, streamURL);
+        soundmanagerCreate(trackID);
         play(tune, trackID, 'Lovebirds ft. Stee Downes - Want You In My Soul', 'i');
       });
 
       $('#i').click(function() {
         var trackID = '235593121'
-        var streamURL = getURL(trackID);
         var tune = event.target;
-
-        soundmanagerCreate(trackID, streamURL);
+        soundmanagerCreate(trackID);
         play(tune, trackID, 'Majid Jordan - Something About You', 's1');
       });
 
       $('#s1').click(function() {
         var trackID = '235427283'
-        var streamURL = getURL(trackID);
         var tune = event.target;
-
-        soundmanagerCreate(trackID, streamURL);
+        soundmanagerCreate(trackID);
         play(tune, trackID, 'LOYAL - Blue and the Green', 't');
       });
 
       $('#t').click(function() {
         var trackID = '208738257'
-        var streamURL = getURL(trackID);
         var tune = event.target;
-
-        soundmanagerCreate(trackID, streamURL);
+        soundmanagerCreate(trackID);
         play(tune, trackID, 'Joe Hertz - Ashes Ft LIV', 'm1');
       });
 
-      // SECOND LINE - VIBES
+      // VIBES LINE
 
       $('#m1').click(function() {
         var trackID = '233605980'
-        var streamURL = getURL(trackID);
         var tune = event.target;
-
-        soundmanagerCreate(trackID, streamURL);
+        soundmanagerCreate(trackID);
         play(tune, trackID, 'Couros - Turning', 'e')
       });
 
       $('#e').click(function() {
         var trackID = '225425321'
-        var streamURL = getURL(trackID);
         var tune = event.target;
-
-        soundmanagerCreate(trackID, streamURL);
+        soundmanagerCreate(trackID);
         play(tune, trackID, 'DREWXHILL - Struggle', 'r1')
       });
 
       $('#r1').click(function() {
         var trackID = '226404391'        
-        var streamURL = getURL(trackID);
         var tune = event.target;
-
-        soundmanagerCreate(trackID, streamURL);
+        soundmanagerCreate(trackID);
         play(tune, trackID, 'Camel Power Club - Ourson', 'r2');
       });
 
       $('#r2').click(function() {
         var trackID = '229055409'
-        var streamURL = getURL(trackID);
         var tune = event.target;
-
-        soundmanagerCreate(trackID, streamURL);
+        soundmanagerCreate(trackID);
         play(tune, trackID, 'Club Kuru - Layla', 'y');
       });
 
       $('#y').click(function() {
         var trackID = '230917974'
-        var streamURL = getURL(trackID);
         var tune = event.target;
-
-        soundmanagerCreate(trackID, streamURL);
+        soundmanagerCreate(trackID);
         play(tune, trackID, 'Luke Mahony Hammond - DRIFT');
       });
     },
--- public/javascripts/days/tuesday.js
+++ public/javascripts/days/tuesday.js
@@ -11,111 +11,87 @@ $(document).ready(function() {
 
       $('#t-tues').click(function() {
         var trackID = '243051362'
-        var streamURL = getURL(trackID);
         var tune = event.target;
-
-        soundmanagerCreate(trackID, streamURL);
+        soundmanagerCreate(trackID);
         play(tune, trackID, 'Tourist - To Have You Back', 'u-tues');
       });
 
       $('#u-tues').click(function() {
         var trackID = '118894880'
-        var streamURL = getURL(trackID);
         var tune = event.target;
-
-        soundmanagerCreate(trackID, streamURL);
+        soundmanagerCreate(trackID);
         play(tune, trackID, 'Odesza - My Friends Never Die Little', 'e-tues');
       });
 
       $('#e-tues').click(function() {
         var trackID = '1688282'
-        var streamURL = getURL(trackID);
         var tune = event.target;
-
-        soundmanagerCreate(trackID, streamURL);
+        soundmanagerCreate(trackID);
         play(tune, trackID, 'Little People - Start Shootin\'', 's-tues');
       });
 
       $('#s-tues').click(function() {
         var trackID = '73799584'
-        var streamURL = getURL(trackID);
         var tune = event.target;
-
-        soundmanagerCreate(trackID, streamURL);
+        soundmanagerCreate(trackID);
         play(tune, trackID, 'Nuages - Dreams', 'd-tues');
       });
 
       $('#d-tues').click(function() {
         var trackID = '154829271'
-        var streamURL = getURL(trackID);
         var tune = event.target;
-
-        soundmanagerCreate(trackID, streamURL);
+        soundmanagerCreate(trackID);
         play(tune, trackID, 'Nao Vs. A. K. Paul - So Good', 'a-tues');
       });
 
       $('#a-tues').click(function() {
         var trackID = '70430214'
-        var streamURL = getURL(trackID);
         var tune = event.target;
-
-        soundmanagerCreate(trackID, streamURL);
+        soundmanagerCreate(trackID);
         play(tune, trackID, 'Nujabes - Reflection Eternal', 'y-tues');
       });
 
       $('#y-tues').click(function() {
         var trackID = '210606695'
-        var streamURL = getURL(trackID);
         var tune = event.target;
-
-        soundmanagerCreate(trackID, streamURL);
+        soundmanagerCreate(trackID);
         play(tune, trackID, 'Tom Misch - Sunshine', 'v-tues');
       });
 
-      // SECOND LIND - VIBES
+      // VIBES LINE
 
       $('#v-tues').click(function() {
         var trackID = '237386464'
-        var streamURL = getURL(trackID);
         var tune = event.target;
-
-        soundmanagerCreate(trackID, streamURL);
+        soundmanagerCreate(trackID);
         play(tune, trackID, 'Pop Caan - Feisty Chat', 'i-tues');
       });
 
       $('#i-tues').click(function() {
         var trackID = '241511581'
-        var streamURL = getURL(trackID);
         var tune = event.target;
-
-        soundmanagerCreate(trackID, streamURL);
+        soundmanagerCreate(trackID);
         play(tune, trackID, 'Woods - Sun City Creeps', 'b-tues');
       });
 
       $('#b-tues').click(function() {
         var trackID = '237101589'
-        var streamURL = getURL(trackID);
         var tune = event.target;
-
-        soundmanagerCreate(trackID, streamURL);
+        soundmanagerCreate(trackID);
         play(tune, trackID, 'Flybear - The Hook', 'ee-tues');
       });
 
       $('#ee-tues').click(function() {
         var trackID = '116385682'
-        var streamURL = getURL(trackID);
         var tune = event.target;
-
-        soundmanagerCreate(trackID, streamURL);
+        soundmanagerCreate(trackID);
         play(tune, trackID, 'Darkside - Paper Trails (RA Sessions)', 'ss-tues');
       });
 
       $('#ss-tues').click(function() {
         var trackID = '201557891'
-        var streamURL = getURL(trackID);
         var tune = event.target;
-
-        soundmanagerCreate(trackID, streamURL);
+        soundmanagerCreate(trackID);
         play(tune, trackID, 'Glxy - Searching You', 't-tues');
       });
     }
