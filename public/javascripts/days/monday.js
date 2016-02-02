5bb3523a29a4e8618d7e191a9cb634c9aa34508d
imarkwick <izzymarkwick@gmail.com>
2016-02-02 12:49:45 +0000

    tidying

--- public/javascripts/days/monday.js
+++ public/javascripts/days/monday.js
@@ -10,42 +10,42 @@ $(document).ready(function() {
       // TOP LINE - MONDAY
 
       $('#c').click(function() {
-        var trackID = '235758881'
+        var trackID = '235758881';
         var tune = event.target;
         soundmanagerCreate(trackID);
         play(tune, trackID, 'Ray BLK - 50/50', 'h');
       });
 
       $('#h').click(function() {
-        var trackID = '187929420'
+        var trackID = '187929420';
         var tune = event.target;
         soundmanagerCreate(trackID);
         play(tune, trackID, 'Ménage à Trois - Ocean Boy', 'r3');
       });
 
       $('#r3').click(function() {
-        var trackID = '24895618'
+        var trackID = '24895618';
         var tune = event.target;
         soundmanagerCreate(trackID);
         play(tune, trackID, 'Lovebirds ft. Stee Downes - Want You In My Soul', 'i');
       });
 
       $('#i').click(function() {
-        var trackID = '235593121'
+        var trackID = '235593121';
         var tune = event.target;
         soundmanagerCreate(trackID);
         play(tune, trackID, 'Majid Jordan - Something About You', 's1');
       });
 
       $('#s1').click(function() {
-        var trackID = '235427283'
+        var trackID = '235427283';
         var tune = event.target;
         soundmanagerCreate(trackID);
         play(tune, trackID, 'LOYAL - Blue and the Green', 't');
       });
 
       $('#t').click(function() {
-        var trackID = '208738257'
+        var trackID = '208738257';
         var tune = event.target;
         soundmanagerCreate(trackID);
         play(tune, trackID, 'Joe Hertz - Ashes Ft LIV', 'm1');
@@ -54,35 +54,35 @@ $(document).ready(function() {
       // VIBES LINE
 
       $('#m1').click(function() {
-        var trackID = '233605980'
+        var trackID = '233605980';
         var tune = event.target;
         soundmanagerCreate(trackID);
         play(tune, trackID, 'Couros - Turning', 'e')
       });
 
       $('#e').click(function() {
-        var trackID = '225425321'
+        var trackID = '225425321';
         var tune = event.target;
         soundmanagerCreate(trackID);
         play(tune, trackID, 'DREWXHILL - Struggle', 'r1')
       });
 
       $('#r1').click(function() {
-        var trackID = '226404391'        
+        var trackID = '226404391';       
         var tune = event.target;
         soundmanagerCreate(trackID);
         play(tune, trackID, 'Camel Power Club - Ourson', 'r2');
       });
 
       $('#r2').click(function() {
-        var trackID = '229055409'
+        var trackID = '229055409';
         var tune = event.target;
         soundmanagerCreate(trackID);
         play(tune, trackID, 'Club Kuru - Layla', 'y');
       });
 
       $('#y').click(function() {
-        var trackID = '230917974'
+        var trackID = '230917974';
         var tune = event.target;
         soundmanagerCreate(trackID);
         play(tune, trackID, 'Luke Mahony Hammond - DRIFT');
